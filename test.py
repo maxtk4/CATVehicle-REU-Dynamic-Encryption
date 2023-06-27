ee886ae1e7d73fa60a8c866ef74c5000aadf6aab
maxtk4 <67165280+maxtk4@users.noreply.github.com>
2023-06-27 15:20:23 -0700

    edited test.py to print classes, created some fake data to play with

--- test.py
+++ test.py
@@ -2,9 +2,11 @@ from ultralytics import YOLO
 from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 import cv2
 
+model = YOLO('yolov8n.pt')
+#model = YOLO('yolov8m.pt')
 
-model = YOLO('yolov8m.pt')
+#results = model('traffic-detection-test.jpg', save=True)
 
-results = model('traffic-detection-test.jpg', save=True)
+#print(results[0].boxes)
 
-print(results[0].boxes)
+print(model.names)
