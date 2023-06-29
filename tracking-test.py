bfccd9d2b9c4096e3db9922335baa8ccbfd38487
maxtk4 <67165280+maxtk4@users.noreply.github.com>
2023-06-28 17:39:06 -0700

    Updated to run on a different video for tracking testing purposes.

--- tracking-test.py
+++ tracking-test.py
@@ -6,5 +6,5 @@ model = YOLO('yolov8n.pt')  # load an official detection model
 #model = YOLO('path/to/best.pt')  # load a custom model
 
 # Track with the model
-results = model.track(source="vid1.mov", save=True) 
+results = model.track(source="vid2.mov", save=True) 
 #results = model.track(source="https://youtu.be/Zgi9g1ksQHc", show=True, tracker="bytetrack.yaml") 
