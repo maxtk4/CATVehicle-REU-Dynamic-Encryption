c31162065bc5169ef029c56296f1b8830d993b32
maxtk4 <67165280+maxtk4@users.noreply.github.com>
2023-07-06 14:44:05 -0700

    Testing with videos from an actual car

--- validate.py
+++ validate.py
@@ -3,6 +3,6 @@ def validate(codea: str, codeb: str):
     """
     Calculates the hamming distance between codea and codeb
     Returns the percentage of disagreements between the codes
-        hamming distance / length of code
+        (length - hamming distance) / length of code
     """
     return (len(codea) - sum([1 if codea[index] != codeb[index] else 0 for index in range(len(codea))])) / len(codea)
